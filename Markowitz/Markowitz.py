222a0ae3ea2200f4d8646a3362ff3acfb8d36a14
Jhonattanln <63247251+Jhonattanln@users.noreply.github.com>
2020-07-05 15:29:50 -0300

    Update Markowitz.py

--- Markowitz/Markowitz.py
+++ Markowitz/Markowitz.py
@@ -1,1 +1,15 @@
 import pandas as pd
+from pypfopt import risk_models
+from pypfopt import expected_returns
+from pypfopt.efficient_frontier import EfficientFrontier
+
+prbr = pd.read_excel(r'C:\Users\Jhona\OneDrive\Área de Trabalho\PRBR11.xlsx', index_col='Data')
+prbr.sort_index()
+###Expectativa de retornos de mu
+mu = expected_returns.mean_historical_return(prbr)
+
+###Matrix de covariancia]
+sigma = risk_models.sample_cov(prbr)
+
+###Retornos
+retornos = prbr.pct_change()
